cc5ebbb0274f332dbfbdd25a67e215dad06c22d6
mghlb <79663822+mghlb@users.noreply.github.com>
2023-06-08 21:53:39 +0300

    Error handling

--- src/app.js
+++ src/app.js
@@ -2,12 +2,15 @@ import express from 'express'
 import cors from 'cors'
 import fetch from 'node-fetch'
 import scrapList from './scrapList.js'
+import helmet from 'helmet'
+
 const app = express()
 
+app.use(helmet())
 app.use(cors())
 app.use(express.json())
 
-app.get('/:listId', async (req, res) => {
+app.get('/:listId', async (req, res, next) => {
   const id = req.params.listId
   const url = `https://imdb-api.com/en/API/IMDbList/${process.env.API_KEY}/${id}`
   try {
@@ -15,19 +18,24 @@ app.get('/:listId', async (req, res) => {
     const data = await response.json()
     res.json(data)
   } catch {
-    res.status(500).send('Server problems... Try again later.')
+    next(err)
   }
 })
 
-app.get('/:userId/list/:listId', async (req, res) => {
+app.get('/:userId/list/:listId', async (req, res, next) => {
   const {userId, listId} = req.params
   const params = `${userId}/list/${listId}`
   try {
     const data = await scrapList(params)
     res.json(data)
-  } catch (error) {
-    res.status(500).send(error)
+  } catch (err) {
+    next(err)
   }
+})
+
+app.use((err, req, res, next) => {
+  console.error(err)
+  res.status(500).send('Error!')
 })
 
 export default app
